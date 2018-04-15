bbad9ff780b2de4dca1fa1d37f959aba4d41a5ef
Nícolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2018-04-15 11:57:14 -0300

    refactor: add index components to facilitate imports

---
+++ app/components/index.js
@@ -0,0 +1,6 @@
+import DaysOfWeek from './DaysOfWeek';
+import HabitCard from './HabitCard';
+import PopupButton from './PopupButton';
+import WeekCalendar from './WeekCalendar';
+
+export { DaysOfWeek, HabitCard, PopupButton, WeekCalendar };
